59f6ac167c46226aea7c4bc1149b610d98692f22
sebastian2023oca-oss <sebastian2023oca@gmail.com>
2026-06-08 20:16:17 -0500

    fix: prueba de ping

--- src/ping.js
+++ src/ping.js
@@ -1,16 +1,16 @@
 const ping = {
     async ejecutar(sock, mensaje) {
-        const inicio = Date.now()
-        const jid = mensaje.key.remoteJid
+        const inicio = Date.now();
+        const jid = mensaje.key.remoteJid;
 
-        await sock.sendMessage(jid, { text: '...' })
+        await sock.sendMessage(jid, { text: '...' });
 
-        const tiempo = Date.now() - inicio
+        const tiempo = Date.now() - inicio;
 
         await sock.sendMessage(jid, {
             text: `🏓 *¡Pong!* El bot está activo.\n\n⚡ Tiempo de reacción: *${tiempo}ms*`
-        })
+        });
     }
-}
+};
 
-export default ping
+export default ping;
